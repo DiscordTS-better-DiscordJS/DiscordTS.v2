c0ea5ac591012ba42c5affe7ea359b86ab34ed03
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-20 13:34:30 +0200

    first look of ws manager

--- src/websocket/WebSocket.ts
+++ src/websocket/WebSocket.ts
@@ -1,7 +1,33 @@
 import EventEmitter from 'https://deno.land/std@0.84.0/node/events.ts'
+import WebSocket from 'https://deno.land/x/websocket@v0.0.6/mod.ts'
 
+import { LINKS } from './links.ts'
+
+/**
+ * @name WebSocketManager - Class to manage discord ws
+ */
 export default class WebSocketManager extends EventEmitter {
 
-    
+    private debugMode: boolean
+    private token: string
+    private isReconnect: boolean
+    public socket: WebSocket
+    private sequence: number
+    private sessionID: number
+
+    constructor(isReconnect: boolean, token: string) {
+        super();
+        this.debugMode = true;
+        this.token = token;
+        this.isReconnect = isReconnect;
+        this.sequence = 0;
+        this.sessionID = 0;
+        this.socket = new WebSocket(LINKS.GATEWAY);
+
+        this.socket?.addEventListener('open', () => {
+            this.debugMode && console.log(`[WS]: WebSocket send 'OPEN'`);
+        });
+
+    }
 
 }
