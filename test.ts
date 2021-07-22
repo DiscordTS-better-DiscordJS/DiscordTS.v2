4bac0d43eadd9bf16ae9ead2a24b68cb2fa941c7
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-22 13:13:19 +0200

    test.ts changes

--- test.ts
+++ test.ts
@@ -4,24 +4,11 @@ import {
     Client, Message, Embed
 } from './mod.ts'
 
-const client = new Client({
-    bot: true // optional, default this options value is true
-});
+const client = new Client();
 
 client.on('message', async (m: Message) => {
 
     if (/@test/gmi.test(m.content)) {
-
-        await m.channel.send(
-            new Embed({
-                description: [
-                    `${m.channel.name}`,
-                    `${client.channels.get('867003800673714186').name}`
-                ].join('\n')
-            })
-        )
-
-    } else if (/@demo/gmi.test(m.content)) {
 
         const args = m.args({ prefix: '@', includeCommandName: true });
 
@@ -30,6 +17,16 @@ client.on('message', async (m: Message) => {
             title: 'Nazwa kanalu z ID',
             description: `${client.channels.get(args[0]).name}`
         }));
+
+    } else if (/@ram/gmi.test(m.content)) {
+
+        const p = Deno.run({ cmd: ['Deno.memoryUsage().rss'] });
+        const { code } = await p.status();
+        const rawOutput = await p.output();
+
+        return m.reply(new Embed({
+            description: `${rawOutput} MB`
+        }))
 
     }
 
