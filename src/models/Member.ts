e27aeca08501fab1d1fcc035a15adef8c5e598bb
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-23 11:13:35 +0200

    ok

---
+++ src/models/Member.ts
@@ -0,0 +1,31 @@
+import { Client } from './Client.ts';
+
+/**
+ * todo Skonczyc to XD
+ */
+export class Member {
+
+    // public nickname: string
+    // public joinedAt: string
+    // public mute: boolean
+    // public deaf: boolean
+    // public guildID: boolean
+    // public premiumSience!: string
+    // public passed!: boolean
+    // public fetched: any
+
+    constructor (data: any, client: Client) {
+
+        // this.nickname = '';
+        // this.joinedAt = data.member_joined_at;
+        // this.deaf = data.member_deaf;
+        // this.mute = data.member_mute;
+        // this.premiumSience = data.
+
+    }
+
+    // get roles
+    // get permissions
+    // get user
+
+}
--- test.ts
+++ test.ts
@@ -17,14 +17,8 @@ client.on('message', async (m) => {
 
     if (/@test/gmi.test(m.content)) {
 
-        const args = m.args({ prefix: '@', includeCommandName: true });
-
-        if (!client.channels.has(args[0])) return m.channel.send('Nie mam takiego kanalu');
-        else return m.reply(new Embed({
-            title: 'Nazwa kanalu z ID',
-            description: `${client.channels.get(args[0]).name}`
-        }));
-
+        console.log()
+        
     } else if (/@ram/gmi.test(m.content)) {
 
         const memoryUsage = client.memoryUsage;
