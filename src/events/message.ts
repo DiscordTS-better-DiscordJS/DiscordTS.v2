8048d9ea551c6a2e9303cf0956d583113d582779
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-23 22:17:36 +0200

    members message filter

--- src/events/message.ts
+++ src/events/message.ts
@@ -4,15 +4,17 @@ import { fetchMember } from '../fetch/methods/member.ts';
 
 export const _ = async (data: any, client: Client) => {
 
-    const message = new Message(data, client);
-    const gID = message.guild.id;
-    const uID = message.author.id;
-    if (!client._memebrs.has(gID, uID)) {
-        if (uID == '671797608750252040') return;
-        const member = await fetchMember(gID, uID);
-        client._memebrs.addOne(gID, member);
+    if (data.type == 'DEFAULT') {
+        const message = new Message(data, client);
+        const gID = message.guild.id;
+        const uID = message.author.id;
+        if (!client._memebrs.has(gID, uID)) {
+            if (uID == '671797608750252040') return;
+            const member = await fetchMember(gID, uID);
+            client._memebrs.addOne(gID, member);
+        }
+
+        return message;
     }
-
-    return message;
-
+    
 }
