df0528cc13f07ae08a22d1a6d9a801f4c08ec8a7
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-25 10:59:08 +0200

    exports *

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@ export * from './src/models/Channel.ts';
 export * from './src/models/Guild.ts';
 export * from './src/models/Collection.ts';
 export * from './src/models/User.ts';
+export * from './src/models/Member.ts';
 export * from './src/models/Permissions/PermissionsBitField.ts';
 export * from './src/models/Permissions/Permissions.ts';
 
@@ -15,6 +16,7 @@ export * from './src/types/models/channel.ts';
 export * from './src/types/models/guild.ts';
 export * from './src/types/models/embed.ts';
 export * from './src/types/models/message.ts';
+export * from './src/types/models/user.ts';
 export * from './src/types/permissions/permissions.ts';
 export * from './src/types/permissions/permissionsFlags.ts';
 export * from './src/types/permissions/permissionsEnum.ts';
