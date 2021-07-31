02e4efefa6791fca0d60279362731bf231f17330
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2021-07-31 09:43:43 +0200

    kurczaki.. zmieniajce wersje w mod.ts jak jest commit

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@
  * @authors Mateusz#4711 & dejwidson#0001 & kacperrrooo#8907
  * @git https://github.com/DiscordTS-better-DiscordJS/DiscordTS.v2
  */
-export const version = 'Alpha-0.0.6';
+export const version = 'Alpha-0.0.7b';
 
 export * from './src/websocket/WebSocket.ts';
 export * from './src/fetch/Api.ts';
