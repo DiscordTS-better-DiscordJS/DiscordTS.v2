b8308c7e0dc6c62ee52ebf6ff0d920d534f9d853
dejwidson2137 <dawid.szewczyk678@gmail.com>
2021-08-05 21:21:38 +0200

    Alpha-0.1.3

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@
  * @authors Mateusz#4711 & dejwidson#0001 & kacperrrooo#8907
  * @git https://github.com/DiscordTS-better-DiscordJS/DiscordTS.v2
  */
-export const version = 'Alpha-0.1.2';
+export const version = 'Alpha-0.1.3';
 
 export * from './src/websocket/WebSocket.ts';
 export * from './src/fetch/Api.ts';
